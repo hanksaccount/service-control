4a12e85c38a11f548b6a14e53f99801eff48e187
Henrik Litmanen <52886886+hanksaccount@users.noreply.github.com>
2026-05-01 21:17:43 +0200

    feat: add status and monitor widgets

--- app/src/main/java/com/litman/servicecontrol/widget/WidgetUpdater.kt
+++ app/src/main/java/com/litman/servicecontrol/widget/WidgetUpdater.kt
@@ -15,10 +15,30 @@ object WidgetUpdater {
         return manager.getAppWidgetIds(component)
     }
 
+    fun statusWidgetIds(context: Context): IntArray {
+        val appContext = context.applicationContext
+        val manager = AppWidgetManager.getInstance(appContext)
+        val component = ComponentName(appContext, ServiceStatusWidgetReceiver::class.java)
+        return manager.getAppWidgetIds(component)
+    }
+
+    fun monitorWidgetIds(context: Context): IntArray {
+        val appContext = context.applicationContext
+        val manager = AppWidgetManager.getInstance(appContext)
+        val component = ComponentName(appContext, ServiceMonitorWidgetReceiver::class.java)
+        return manager.getAppWidgetIds(component)
+    }
+
     fun refresh(context: Context) {
         val appContext = context.applicationContext
-        val ids = appWidgetIds(appContext)
-        Log.d(TAG, "[ServiceCtrl] WidgetUpdater: refreshing classic widgets count=${ids.size}")
-        ServiceWidget.update(appContext, AppWidgetManager.getInstance(appContext), ids)
+        val manager = AppWidgetManager.getInstance(appContext)
+        val classicIds = appWidgetIds(appContext)
+        val statusIds = statusWidgetIds(appContext)
+        val monitorIds = monitorWidgetIds(appContext)
+
+        Log.d(TAG, "[ServiceCtrl] WidgetUpdater: classic=${classicIds.size} status=${statusIds.size} monitor=${monitorIds.size}")
+        ServiceWidget.update(appContext, manager, classicIds)
+        ServiceStatusWidget.update(appContext, manager, statusIds)
+        ServiceMonitorWidget.update(appContext, manager, monitorIds)
     }
 }
